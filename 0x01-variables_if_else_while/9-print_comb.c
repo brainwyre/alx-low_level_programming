453ac355ac30beb02903acd9c2eb6a2a0901387d
Jeremiah AGBITA <jerryagbita@yagmail.com>
2023-02-20 18:13:50 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar('.');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
