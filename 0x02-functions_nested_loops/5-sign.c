6888c1d6ec728e14970f89e8ced1b343dace774c
Jeremiah AGBITA <jerryagbita@yagmail.com>
2023-02-22 10:22:11 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - functions that prints signs
+ * @n: function parameter
+ *
+ * Return: 1, 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
